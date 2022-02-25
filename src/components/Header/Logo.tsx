961c0d2ff82221d84f4503fb912b648c586fae4a
captaincole <andrew.thielcole@gmail.com>
2022-02-25 16:13:44 -0300

    Updating favicon

--- src/components/Header/Logo.tsx
+++ src/components/Header/Logo.tsx
@@ -2,6 +2,6 @@ import React from 'react'
 
 export const Logo = () => {
     return <div>
-        <img alt="Orbit DB" height={'60px'} src="https://orbitdb.org/images/orbit_db_logo_color.png" />
+        <img alt="Orbit DB" height={'60px'} src="orbit_db_logo.png" />
     </div>
 }
